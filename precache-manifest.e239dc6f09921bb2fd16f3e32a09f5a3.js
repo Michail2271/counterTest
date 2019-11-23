8d55edb1ab0041b96432740be93c364db636b0fc
Michail2271 <misha2271@icloud.com>
2019-11-23 20:18:12 +0300

    Updates

---
+++ precache-manifest.e239dc6f09921bb2fd16f3e32a09f5a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64059592dde19977f74b38c28fbf2506",
+    "url": "/counterTest/index.html"
+  },
+  {
+    "revision": "de1bd57c5d3a4aa44f59",
+    "url": "/counterTest/static/css/main.3a3cd944.chunk.css"
+  },
+  {
+    "revision": "ca69042d03fe7f1ccba9",
+    "url": "/counterTest/static/js/2.635489c1.chunk.js"
+  },
+  {
+    "revision": "de1bd57c5d3a4aa44f59",
+    "url": "/counterTest/static/js/main.e700e2bd.chunk.js"
+  },
+  {
+    "revision": "6628935d766971dd37a5",
+    "url": "/counterTest/static/js/runtime-main.db5d7d86.js"
+  }
+]);
